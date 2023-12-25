3e3578d73ecd6edc3f5fff64c367cca81dd8c0c0
odins <odins41@yandex.ru>
2023-12-25 19:26:28 +0300

    Добавил параметры для запроса

--- get_image.py
+++ get_image.py
@@ -3,11 +3,11 @@ import requests
 from split_file_name import *
 
 
-def download_image(url, folder, photo_number, photo_name):
+def download_image(url, folder, photo_number, photo_name, payload):
     os.makedirs(folder, exist_ok=True)
     file_ext = split_file_name(url)
     filename = "{0}_{1}{2}".format(photo_name, photo_number, file_ext)
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
     path = os.path.join(folder, filename)
     with open(path, "wb") as file:
